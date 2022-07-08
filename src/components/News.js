57dd3c197ffade231ecac03226d6a85409da19fc
Swasti Bhagora <64397504+Swasti877@users.noreply.github.com>
2022-07-08 12:49:43 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -19,6 +19,8 @@ export default function News(props) {
     useEffect(() => {
         document.title = `${capitalizeFirstLetter(props.category)} - News WebApp`
         handle();
+        
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const handle = async () => {
